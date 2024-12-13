11c6ef0e6849d19ddc2e0c696322c70c56f4f1c8
ShernOC <sherlynea1994@hotmail.com>
2024-12-13 23:55:06 +0300

    sql data base retrieve/select

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO
+INSERT INTO bears(name, age, sex, color, temperament,   alive) VALUES
+("Mr. Chocolate", 4, "M", "Green", "Happy", 1),
+("Rowdy", 34, "F", "Orange", "Sad",  0),
+("Tabitha", 50, "M", "Blue",  "Ugly", 1),
+("Sergeant Brown", 20, "M", "Gray", "Neutral",1),
+("Melissa", 90, "M", "Brown", "Excited", 0),
+("Grinch", 16, "F", "Pink", "Happy", 1),
+("Wendy", 24, "M", "Yellow", "Confused", 0),
+(NULL,20, "M", "black", "aggressive", 0 );
